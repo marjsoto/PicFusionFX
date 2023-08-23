0b0e20af47235ecd457337443ef1daac7dd4da12
mscedeno <mscedeno@espol.edu.ec>
2023-08-23 01:35:58 -0500

    interfaz mejorada

--- app/src/main/java/com/example/proyectocamara/MainActivity2.java
+++ app/src/main/java/com/example/proyectocamara/MainActivity2.java
@@ -12,25 +12,35 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
     Button btnCamara;
     ImageView visor;
+    CharSequence text;
+    int duration;
+    Bundle extras;
+    Bitmap imgBitmap;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        text = "Imagen capturada con exito";
+        duration = Toast.LENGTH_SHORT;
+
         TextView txtUser = (TextView) findViewById(R.id.nomuser);
         Bundle bundle = getIntent().getExtras();
         txtUser.setText(bundle.getString("usuario"));
 
         btnCamara = findViewById(R.id.btncam);
-        visor = findViewById(R.id.imageV);
+        //visor = findViewById(R.id.imageV);
 
         btnCamara.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,16 +54,24 @@ public class MainActivity2 extends AppCompatActivity {
     //este codigo permite abrir la camara
     ActivityResultLauncher<Intent> camaraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
-        //permite visualizar la imagen tomada
+        //permite visualizar la imagen tomada en la siguiente pantalla
         public void onActivityResult(ActivityResult result) {
             if(result.getResultCode()==RESULT_OK){
-                Bundle extras = result.getData().getExtras();
-                Bitmap imgBitmap = (Bitmap) extras.get("data");
-                visor.setImageBitmap(imgBitmap);
+                extras = result.getData().getExtras();
+                imgBitmap = (Bitmap) extras.get("data");
+
+                toast = Toast.makeText(MainActivity2.this, text, duration);
+                toast.show();//muestra un pequeño mensaje al tomar una foto con exito
             }
         }
     });
 
+    public void siguiente(View view) {
+        Intent i = new Intent(this, MainActivity4.class );
+        extras.putParcelable("Bitmap", imgBitmap);
+        i.putExtras(extras);//envia la imagen bitmap capturada al main activity 4
+        startActivity(i);//lleva a la siguiente activity
+    }
 
     public void cerraSesion(View view) {
         Intent i = new Intent(this, MainActivity.class );
--- app/src/main/java/com/example/proyectocamara/MainActivity4.java
+++ app/src/main/java/com/example/proyectocamara/MainActivity4.java
@@ -2,13 +2,35 @@ package com.example.proyectocamara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    ImageView visora;
+    ImageView visord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        visora = findViewById(R.id.imgAntes);
+        Bundle extras = getIntent().getExtras();//obtiene el bitmap para mostrarlo en el Image View
+        Bitmap bmp = (Bitmap) extras.getParcelable("Bitmap");
+        visora.setImageBitmap(bmp);
+    }
+
+    public void volver(View view) {
+        Intent i = new Intent(this, MainActivity2.class );
+        startActivity(i);
+    }
+
+    public void cerraSesion(View view) {
+        Intent i = new Intent(this, MainActivity.class );
+        startActivity(i);
     }
 }
