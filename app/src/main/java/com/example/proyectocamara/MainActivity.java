f565a01136da1e8b55b7c5182e9c1cc2c2481d58
Marcelo Sotomayor <marjsoto@espol.edu.ec>
2023-08-24 03:38:22 -0500

    error

--- app/src/main/java/com/example/proyectocamara/MainActivity.java
+++ app/src/main/java/com/example/proyectocamara/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
@@ -22,42 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Crear una instancia de Retrofit
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://localhost:5000") // Reemplaza BASE_URL con la URL de tu servidor Flask
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        // Crear una instancia de la interfaz ApiService
-        ApiService apiService = retrofit.create(ApiService.class);
-
-        // Crear una solicitud JSON
-        ImageProcessingRequest request = new ImageProcessingRequest();
-        request.setPrompt("Tu prompt aquí");
-        request.setNegativePrompt("Tu negative prompt aquí");
-        request.setImagePath("Ruta de tu imagen aquí");
-
-        // Enviar la solicitud al servidor para procesar la imagen
-        Call<ImageProcessingResponse> call = apiService.generateImage(request);
-        call.enqueue(new Callback<ImageProcessingResponse>() {
-            @Override
-            public void onResponse(Call<ImageProcessingResponse> call, Response<ImageProcessingResponse> response) {
-                if (response.isSuccessful()) {
-                    // La respuesta exitosa contiene la ruta de la imagen procesada
-                    String outputPath = response.body().getOutputPath();
-
-                    // Cargar y mostrar la imagen procesada con Picasso
-                    Picasso.get().load("https://localhost:5000" + "/get_image?output_path=" + outputPath).into(MainActivity4.class.imgDespues);
-                } else {
-                    // Manejar errores aquí
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ImageProcessingResponse> call, Throwable t) {
-                // Manejar errores de red aquí
-            }
-        });
     }
 
     public void inicio(View view) {
--- app/src/main/java/com/example/proyectocamara/MainActivity2.java
+++ app/src/main/java/com/example/proyectocamara/MainActivity2.java
@@ -17,6 +17,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.gson.annotations.SerializedName;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -64,6 +71,61 @@ public class MainActivity2 extends AppCompatActivity {
                 toast = Toast.makeText(MainActivity2.this, text, duration);
                 toast.show();//muestra un pequeño mensaje al tomar una foto con exito
             }
+            // Crear una instancia de Retrofit
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://localhost:5000") // Reemplaza BASE_URL con la URL de tu servidor Flask
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            // Crear una instancia de la interfaz ApiService
+            ApiService apiService = retrofit.create(ApiService.class);
+
+            // Crear una solicitud JSON
+            // Obtener los valores de los campos de entrada
+            // Obtener los valores de los campos de entrada desde activity_main2.xml
+            EditText edtTextoUser = findViewById(R.id.edtTextoUser);
+            EditText edtTextoUser2 = findViewById(R.id.edtTextoUser2);
+
+
+            // Obtener la ruta de la imagen (debes proporcionar la ruta real aquí)
+            String imagePath = "Ruta de tu imagen aquí";
+
+            // Obtener los textos de los campos de entrada
+            String promptPositivo = edtTextoUser.getText().toString();
+            String promptNegativo = edtTextoUser2.getText().toString();
+
+            // Crear una solicitud JSON con los valores adecuados
+            ImageProcessingRequest request = new ImageProcessingRequest(promptPositivo, promptNegativo, imagePath);
+
+            // Enviar la solicitud al servidor para procesar la imagen
+            Call<ImageProcessingResponse> call = apiService.generateImage(request);
+
+
+            // Enviar la solicitud al servidor para procesar la imagen
+            call = apiService.generateImage(request);
+            call.enqueue(new Callback<ImageProcessingResponse>() {
+                @Override
+                public void onResponse(Call<ImageProcessingResponse> call, Response<ImageProcessingResponse> response) {
+                    if (response.isSuccessful()) {
+                        // La respuesta exitosa contiene la ruta de la imagen procesada
+                        String outputPath = response.body().getOutputPath();
+
+                        // Obtén una referencia al ImageView
+                        ImageView imgDespues = findViewById(R.id.imgDespues);
+
+                        // Cargar y mostrar la imagen procesada con Picasso
+                        Picasso.get().load("https://localhost:5000" + "/get_image?output_path=" + outputPath).into(imgDespues);
+                    } else {
+                        // Manejar errores aquí
+                    }
+                }
+
+
+                @Override
+                public void onFailure(Call<ImageProcessingResponse> call, Throwable t) {
+                    // Manejar errores de red aquí
+                }
+            });
         }
     });
 
